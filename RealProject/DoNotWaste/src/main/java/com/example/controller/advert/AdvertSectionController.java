a1020079c1579dd7cfb1d6208e2d4c1bc6d939b6
Alisher <alisherodilov482@gmail.com>
2023-07-17 04:47:20 -0700

    AdvertCategory is updated !

---
+++ RealProject/DoNotWaste/src/main/java/com/example/controller/advert/AdvertSectionController.java
@@ -0,0 +1,39 @@
+package com.example.controller.advert;
+
+import com.example.dto.advert.AdvertSectionCreationDTO;
+import com.example.service.AdvertSectionService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Author: Alisher Odilov
+ * Date: 21.06.2023
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping("/advertSection")
+public class AdvertSectionController {
+
+    private final AdvertSectionService advertSectionService;
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody AdvertSectionCreationDTO dto){
+        AdvertSectionCreationDTO result=advertSectionService.create(dto);
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@RequestBody AdvertSectionCreationDTO dto, @PathVariable("id") Integer id){
+        AdvertSectionCreationDTO result=advertSectionService.update(dto,id);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Integer id){
+      Boolean result=advertSectionService.deleteById(id);
+      return ResponseEntity.ok(result);
+    }
+
+
+}
+
