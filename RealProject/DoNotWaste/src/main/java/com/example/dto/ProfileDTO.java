62d117efbab87f79c3212367de8e4b49b5df7711
Alisher <alisherodilov482@gmail.com>
2023-07-24 01:08:41 -0700

    PhoneValidation was added !

--- RealProject/DoNotWaste/src/main/java/com/example/dto/ProfileDTO.java
+++ RealProject/DoNotWaste/src/main/java/com/example/dto/ProfileDTO.java
@@ -2,6 +2,10 @@ package com.example.dto;
 
 import com.example.enums.ProfileRole;
 import com.example.enums.Status;
+import com.example.validation.validationInterface.ValidPhone;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -12,10 +16,16 @@ import java.time.LocalDateTime;
 @Data
 public class ProfileDTO{
     private  Long id;
+    @NotBlank(message = "Name should not be null ! ")
     private  String name;
+    @NotBlank(message = "SurName should not be null ! ")
     private  String surname;
+    @Email(message = "Email shoul be valid ! ")
     private  String email;
+    @ValidPhone //Todo o`zim yasadim ichiga kirib ko`ringlar
     private  String phone;
+    @NotBlank
+    @Size(max = 8, min = 8, message = "charachter should be 8 !")
     private  String password;
     private  ProfileRole role;
     private  Status status;
